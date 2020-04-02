6a1b315b73f6b6222b6397b4f79c02447b33d20a
Samuel Glez <samuelgonzalezalonso@gmail.com>
2020-04-02 17:01:46 +0100

    Cambio de plantilla de mapa

--- src/views/Map/index.tsx
+++ src/views/Map/index.tsx
@@ -27,8 +27,8 @@ export const Mapa: FC = observer(() => {
          //PENDIENTE EXPLICACION RAUL
       >
          <TileLayer
-            url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors'
             // noWrap="true" //PENDIENTE EXPLICACION RAUL
          />
          {console.log(appStore.maxConfirmed)}
